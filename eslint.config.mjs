09a036f813e2277805e7de0d7755d17635afdea7
raulntjj <rauloliveira5230@gmail.com>
2026-03-05 10:46:54 -0300

    chore: update dependencies for ESLint and TypeScript support

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+
+const eslintConfig = [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "no-console": "warn",
+    },
+  },
+  {
+    ignores: [
+      ".next/**",
+      "node_modules/**",
+      "coverage/**",
+      "__mocks__/**",
+    ],
+  },
+];
+
+export default eslintConfig;
